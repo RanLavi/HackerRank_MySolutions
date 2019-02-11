99f123795c63db72feeee902dec85f3318f9667a
RanLavi <43575731+RanLavi@users.noreply.github.com>
2019-02-11 17:51:07 +0200

    Adding 2D Array - DS challenge

---
+++ Interview Preparation Kit/Arrays/2D Array - DS/Solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int hourglassSum(int[][] arr) {
+        /* Start of my code */
+        // Setting max to the minimum value an int can have
+        int max = Integer.MIN_VALUE;
+        // Starting from the second row until the second row from the end
+        for(int i = 1; i < 5; i++){
+            // Starting from the second column until the second column from the end
+            for(int j = 1; j < 5; j++){
+                // Calculating the current hourglass and comparing it to the maximal hourglass so far
+                max = Math.max(max, arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]);
+            }
+        }
+    return max;
+    /* End of my code */
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
